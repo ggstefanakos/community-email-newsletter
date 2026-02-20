053cfe086f3ba296c84cfc6bea914d30f8b9bd21
Yorgos <stefanakosgeorg@gmail.com>
2026-02-20 03:03:19 +0200

    html mails supported

--- email_sender.py
+++ email_sender.py
@@ -3,6 +3,8 @@ import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
+from email.message import EmailMessage
+from email.utils import formataddr
 
 
 load_dotenv()
@@ -31,5 +33,30 @@ def send_email(recipient,subject,body):
     except Exception as e:
         print(f"Error sending mail: {e}")
 
+def send_email_variant(receiver,subject,body,body_html):
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = formataddr(("Robot Cousin",f"{EMAIL_ADDRESS}"))
+    msg["To"] = receiver
+    msg["BCC"] = EMAIL_ADDRESS
+
+    msg.set_content(body)
+
+    msg.add_alternative(body_html,subtype="html")
+
+    with smtplib.SMTP(SMTP_SERVER,SMTP_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+            server.sendmail(EMAIL_ADDRESS,receiver,msg.as_string())
+
 if __name__ == "__main__":
-    send_email("parepoulo91@gmail.com","Test email","This is the first email send by cousin bot.")
+
+    body_html = f"""\
+    <html>
+      <body>
+        <p>This is the <strong>first</strong> email send by cousin bot.</p>
+      </body>
+    </html>
+    """
+    send_email_variant("parepoulo91@gmail.com","Test email","This is the first email send by cousin bot.",body_html)
+
